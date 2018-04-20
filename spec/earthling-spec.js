7de8a99fe58acb52e88003934cae5e79c0727f9b
Dawnabelle <dawn.mott@gmail.com>
2018-04-20 16:25:45 -0700

    add second working test for time between two dates

--- spec/earthling-spec.js
+++ spec/earthling-spec.js
@@ -1,4 +1,4 @@
-import { Earthling } from './../src/earthling';
+import  { Earthling } from './../src/earthling';
 
 describe('Earthling', function() {
   let myEarthling = new Earthling(new Date('Nov, 14, 1981'));
@@ -7,4 +7,12 @@ describe('Earthling', function() {
   it('should return age in seconds as a number', function() {
     expect(typeof myEarthling.ageInSeconds()).toEqual('number');
   });
+
+  it('should return the different between two dates in seconds', function(){
+
+    let timePassed = 1149634800
+    console.log(myEarthling.betweenDatesInSeconds(new Date('Nov, 14, 1981'), new Date('Apr, 20, 2018')));
+
+    expect(myEarthling.betweenDatesInSeconds(new Date('Nov, 14, 1981'), new Date('Apr, 20, 2018'))).toEqual(timePassed);
+  })
 });
--- src/earthling.js
+++ src/earthling.js
@@ -13,6 +13,12 @@ class Earthling {
     // round to nearest absolute number convs mils to secs by div by 1000 leaving no decimal
     return Math.round(Math.abs((ageInMilSeconds - now) / 1000));
   }
+
+  betweenDatesInSeconds(date1, date2){
+    let date1InSeconds = date1.getTime() / 1000;
+    let date2InSeconds = date2.getTime() / 1000;
+    return Math.round(Math.abs(date1InSeconds - date2InSeconds));
+  }
 }
 
 export { Earthling };
