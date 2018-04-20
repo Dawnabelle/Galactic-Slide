518eeb042be76747c307c85d3a3b4cfd6fb9dfe4
Dawnabelle <dawn.mott@gmail.com>
2018-04-20 15:53:35 -0700

    add working function Earthling

--- src/earthling.js
+++ src/earthling.js
@@ -1,13 +1,18 @@
 class Earthling {
+// creates birthdate constructor
   constructor(birthdate){
     this.birthdate = birthdate;
   }
 
-  calcAge(){
-    const date = Date.now();
-    let age = (date - this.birthday);
-    this.age = (age / 33155695200);
+// calculates age in seconds
+  ageInSeconds(){
+    // date.now() calls the current date to the millasecond
+    let now = Date.now();
+    // getTime() calls the users birthday and generates an answer to the second
+    let ageInMilSeconds = this.birthdate.getTime();
+    // round to nearest absolute number convs mils to secs by div by 1000 leaving no decimal
+    return Math.round(Math.abs((ageInMilSeconds - now) / 1000));
   }
 }
 
-export { Earthling }
+export { Earthling };
